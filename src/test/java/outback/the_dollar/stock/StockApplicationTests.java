e8c9039b67fe2c1f54f53cbe0b32ebabc3fc94a5
강동희 <115144342+chocolaggibbiddori@users.noreply.github.com>
2025-12-23 00:20:38 +0900

    feat: stock initial settings

---
+++ src/test/java/outback/the_dollar/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package outback.the_dollar.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
